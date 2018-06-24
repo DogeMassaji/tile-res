d02e800c11b0825b2af9c7749af8c5358e911c18
doge_massaji <dogemassaji@foxmail.com>
2018-06-25 01:18:11 +0800

    tile res

---
+++ demo/demo.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="demo" tilewidth="32" tileheight="32" tilecount="80" columns="2">
+ <image source="map.png" width="64" height="1280"/>
+ <tile id="16">
+  <objectgroup draworder="index" name="grass">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+</tileset>
